2a61cd99e8e2ecc584ac85718f9756a82f059542
Angela Field <angela.field11@gmail.com>
2015-02-15 13:41:19 -0800

    add search partials

--- public/scripts/search.js
+++ public/scripts/search.js
@@ -5,5 +5,15 @@ angular.module('search.controllers', [])
   })
 
   .controller('SearchResultCtrl', function($scope, $http, $stateParams) {
-    $scope
+    $scope.searchResults = function() {
+      var url = 'http://localhost:3000/recipes/search?term=' + $scope.newSearch.term;
+      $http.get(url)
+        .success(function(data) {
+          $scope.recipes = data;
+        });
+    };
+
+    $scope.searches = [];
+    $scope.searchModel = {term: ''};
+    $scope.newSearch = angular.copy($scope.searchModel);
   })
