ef9641981ce3983a92c8e566da59fd2f53e52462
cgardens <giardina.charles@gmail.com>
2015-02-15 16:08:01 -0800

    jury-rigged a call to server/users

--- public/scripts/search.js
+++ public/scripts/search.js
@@ -10,7 +10,8 @@ angular.module('search.controllers', [])
     $scope.newSearch = { term: '' };
 
     $scope.getSearchResults = function() {
-      $http.get('http://localhost:3000/recipes/search?term=' + $scope.newSearch.term)
+      var userID = '54dd42f2e4b0721d2b31da3e'
+      $http.get('http://localhost:3000/users/' + userID)
       .success(function(data) { $scope.recipes = data; })
       .error(function(data) { console.log('Error: ' + data) })
     }
